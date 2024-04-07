fe363c456252883b228d834e6c93c09fd851b678
Tajul Islam Sumon <sumon211420@gmail.com>
2024-04-07 16:34:45 +0600

    New Edited

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,9 +1,9 @@
 import CategoryList from "@/app/components/category";
 
 async function getAllListsByCategory(getId: string) {
-  const res = await fetch(`${process.env.PORT}/api/category?categoryID=${getId}`, {
+  const res = await fetch(`${process.env.URL}/api/category?categoryID=${getId}`, {
     method: "GET",
-    cache: "no-store",
+    cache: "no-cache",
   });
 
   const data = await res.json();
